04ac6d35524b4e6e4c1d1cc036a0a1d8d7223e55
Luciernagas <104604359+Luciernagas@users.noreply.github.com>
2023-07-25 11:00:33 -0500

    :)

--- src/main/java/org/example/testingweb/GreetingService.java
+++ src/main/java/org/example/testingweb/GreetingService.java
@@ -1,2 +1,10 @@
-package org.example.testingweb;public class GreetingService {
+package org.example.testingweb;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String greet() {
+        return "Hello, World";
+    }
 }
